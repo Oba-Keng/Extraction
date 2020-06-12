3d87a98357a1a6d034c559471f1ee264b81b1f09
Obakeng <obeezus@gmail.com>
2020-06-12 09:02:53 +0200

    working on front end

---
+++ extraction/app/app.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const book = "warandpeace.txt";
+
+fs.readFile(book, "utf8", (err, str) => {
+  //turn to lowercase
+  str = cleanString(str);
+
+  const search = stringSplit(str);
+
+  let searchObject = {};
+
+  search.forEach(element => {
+    // empty string
+    if (element.trim() == "") return;
+    searchObject.hasOwnProperty(element)
+      ? searchObject[element]++
+      : (searchObject[element] = 1);
+  });
+
+  let word,
+    count = 0;
+
+  for (const prop in searchObject) {
+    if (searchObject[prop] > count) {
+      word = prop;
+      count = searchObject[prop];
+    }
+  }
+
+  console.log("the word most frequently used is....:" + word);
+  console.log("appearing :" + count + " times");
+});
+
+const stringSplit = str => {
+  return str.split(/\s+/);
+};
+
+const cleanString = str => {
+  const reg = /[.,"();*#\[\]?!@%_“$:”0-9‘]/g;
+  return str.toLowerCase().replace(reg, "");
+};
